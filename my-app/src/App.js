b767f83f4d95b1b958fbdfd99d6f96e8543c5906
Sai Kumar Munagala <talpasaikumar.munagala@gmail.com>
2022-01-30 20:05:57 +0530

    made changes in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Learning with "friends"
         </p>
         <a
           className="App-link"

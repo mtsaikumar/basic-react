03bf835ef88efdee60031e5d35600749a8ca9291
sharao <sharao111@gmail.com>
2022-02-14 22:48:17 +0530

    added Srinivas app content

---
+++ my-app/src/components/SrinivasComponents/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+function Header() {
+  return (
+    <div className="header">
+      <span className="logo">
+
+      </span>
+      <h3>
+          Srinivas Rao Gunja's App
+      </h3>     
+
+    </div>
+  );
+}
+
+export default Header;
